d0a35b26dde2598ed7b4846adfb627cb91dad22b
Almost Llamas <kim.tony.20.20.20@gmail.com>
2021-03-04 13:14:15 +0700

    feat: add event to IStrongAlpaca

--- contracts/6/token/interfaces/IStrongAlpaca.sol
+++ contracts/6/token/interfaces/IStrongAlpaca.sol
@@ -4,4 +4,8 @@ interface IStrongAlpaca {
   function prepareHodl() external;
   function hodl() external;
   function unhodl() external;
+
+  event PrepareHodl(address indexed user, address indexed relayer);
+  event Hodl(address indexed user, address indexed relayer, uint256 receivingStrongAlpacaAmount);
+  event Unhodl(address indexed user, uint256 receivingAlpacaAmount);
 }
